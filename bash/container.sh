c5209a8c896b7a13043486147a16a3771a7bc1a6
Tommy <123013486+200428257@users.noreply.github.com>
2023-02-22 17:51:03 -0500

    Update container.sh

--- bash/container.sh
+++ bash/container.sh
@@ -4,7 +4,6 @@ lxc launch ubuntu:22.04 COMP2101-S22
 hostname='COMP2101-S22'
 lxc network show lxdbr0>>~/COMP2101/bash/containerinfo.txt
 ipaddress=$(awk '/ipv4.address/ {print $2}' ~/COMP2101/bash/containerinfo.txt)
-echo $ipaddress
 sudo -- sh -c "echo '$ipaddress  $hostname'>>/etc/hosts"
 sudo apt install curl
 curl https://COMP2101-S22
