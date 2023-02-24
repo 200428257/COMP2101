06dff634627dba9f075f4475b34f5ddd37c5ca6a
Tommy <200428257@student.georgianc.on.ca>
2023-02-24 09:29:14 -0500

    Script for lab 3

--- bash/container.sh
+++ bash/container.sh
@@ -5,6 +5,8 @@ hostname='COMP2101-S22'
 lxc network show lxdbr0>>~/COMP2101/bash/containerinfo.txt
 ipaddress=$(awk '/ipv4.address/ {print $2}' ~/COMP2101/bash/containerinfo.txt)
 sudo -- sh -c "echo '$ipaddress  $hostname'>>/etc/hosts"
+secondip=$(echo $ipaddress | cut --complement -c 12-14)
+sudo -- sh -c "echo 'nameserver   $secondip'>>/etc/resolv.conf"
 sudo apt install curl
 curl https://COMP2101-S22
 sed -i 'd' ~/COMP2101/bash/containerinfo.txt
