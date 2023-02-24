c03bab5c9db6dbaf1af87ac8dd39db713b3fbd34
Tommy <123013486+200428257@users.noreply.github.com>
2023-02-24 09:45:55 -0500

    Update container.sh

--- bash/container.sh
+++ bash/container.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 snap install lxd
-sudo lxc launch ubuntu:22.04 COMP2101-S22
+lxd init COMP2101-S22
+lxc launch COMP2101-S22
 hostname='COMP2101-S22'
 lxc network show lxdbr0>>~/COMP2101/bash/containerinfo.txt
 ipaddress=$(awk '/ipv4.address/ {print $2}' ~/COMP2101/bash/containerinfo.txt)
