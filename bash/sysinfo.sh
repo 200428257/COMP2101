7bb46f844ed7ce3a4e899346606a8929fb107a1d
Tommy Lalonde <200428257@student.georgianc.on.ca>
2024-04-05 16:36:33 -0400

    Brief and short version of the previous sysinfo command

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,19 +1,31 @@
+
 #!/bin/bash
-#Line 3 is a command that will give information on the host's machine such as the Static hostname, Machine ID, OS, etc....
-hostnamectl
+#Line 3 Stores the output of the hostnamectl command which gives information on the host's machine such as the Static hostname, Machine ID, OS, etc.... Into a variable
+hostinfo=$(hostnamectl)
 #Line 5 is a command that will store the FQDN of the machine into a variable.
 fqdn=$(hostname)
-#Line 7 will display the information stored in the variable.
+echo " "
+echo "Report for myvm"
+echo "===================="
+#Line 10 will display the information stored in the variable.
 echo FQDN: $fqdn
-#Line 9 is similar to line 5 but for the df output.
+#Line 12 is similar to line 5 but for the df output.
 df=$(df -h)
-#Line 11 and 12 are both extracting specific lines of data within the df variable using the Grep command and storing it in their own variables for further use.
-filesystem=$(echo "$df" | grep 'Filesystem')
+#Line 14 and 15 are both extracting specific lines of data within the df variable using the Grep command and storing it in their own variables for further use.
 sda3=$(echo "$df" | grep '/dev/sda3')
-echo "Root Filesystem Status:"
-#Line 15 and 16 does the same as Line 7.
-echo "$filesystem"
-echo "$sda3"
-echo "IP Addresses:"
-#Line 19 displays all the machine IP Adresses, it will show both ipv4 and ipv6. However mine only has a ipv4.
-hostname -I
+freespace=$(echo "$sda3" | awk 'BEGIN {sum=0} {sum=sum+$4} END {print sum}')
+#Line 18 and 19 does the same as Line 10 with different variable.
+echo "Root Filesystem Free Space: $freespace"'G'
+#Line 21 stores all the machine IP Adresses in a variable, it will store both ipv4 and ipv6. However mine only has a ipv4.
+ipaddress=$(hostname -I)
+# Line 23 does the same as Line 10 but with different variable
+echo "Ip Addresse: $ipaddress"
+osinfo=$(hostnamectl)
+osversion=$(echo "$osinfo" | grep 'Operating')
+os=$(echo "$osversion" | awk '{getline} {print $3, $4}')
+distroname=$(echo "$osinfo" | grep 'Kernel')
+distrofullname=$(echo "$distroname" | awk '{getline} {print $2, $3}')
+distro=$(echo "$distrofullname" | cut -b 1-11)
+echo "Operating System name and Version: $os"'/'"$distro"
+echo "===================="
+echo " "
